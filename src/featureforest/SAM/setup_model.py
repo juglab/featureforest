2ea98c96bf5f3486c259fb12ef06e2e089be809f
Mehdi Seifi <mehdiseifi@gmail.com>
2024-06-03 14:48:10 +0200

    added model downloader using pooch

--- src/featureforest/SAM/setup_model.py
+++ src/featureforest/SAM/setup_model.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import torch
 
+from ..utils.downloader import download_model
 from .models import MobileSAM
 
 
@@ -10,13 +11,13 @@ def setup_mobile_sam_model():
     print(f"running on {device}")
     # sam model (light hq sam)
     model = MobileSAM.setup_model().to(device)
-    # load weights
-    weights = torch.load(
-        Path(__file__).parent.joinpath(
-            "./models/weights/mobile_sam.pt"
-        ),
-        map_location=device
+    # download model's weights
+    model_file = download_model(
+        model_url="https://github.com/ChaoningZhang/MobileSAM/raw/master/weights/mobile_sam.pt",
+        model_name="mobile_sam.pt"
     )
+    # load weights
+    weights = torch.load(model_file, map_location=device)
     model.load_state_dict(weights, strict=True)
     model.eval()
 
