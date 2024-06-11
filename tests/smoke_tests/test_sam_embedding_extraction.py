7e8b42d84ab42642b3c5745fb22c1a0897cc20e1
veegalinova <veegalinova@gmail.com>
2024-06-11 16:29:33 +0200

    update embedding test

--- tests/smoke_tests/test_sam_embedding_extraction.py
+++ tests/smoke_tests/test_sam_embedding_extraction.py
@@ -5,15 +5,9 @@ import pytest
 from tqdm import tqdm
 import h5py
 
-from featureforest.SAM import setup_mobile_sam_model
+from featureforest.models import MobileSAM
 from featureforest.utils.data import get_stack_dims, get_patch_size
-from featureforest.utils.extract import get_sam_embeddings_for_slice
-
-
-@pytest.fixture
-def sam_model_and_device():
-    sam_model, device = setup_mobile_sam_model()
-    return sam_model, device
+from featureforest.utils.extract import get_slice_features
 
 
 @pytest.mark.parametrize(
@@ -25,27 +19,25 @@ def sam_model_and_device():
         (np.ones((5, 256, 256, 3)), (25, 64, 64, 320), 5),  # 3D RGB
     ],
 )
-def test_embedding_extraction(
-    test_image, expected_shape, expected_slices, sam_model_and_device
-):
-    sam_model, device = sam_model_and_device
+def test_embedding_extraction(test_image, expected_shape, expected_slices):
+    num_slices, img_height, img_width = get_stack_dims(test_image)
+    patch_size = get_patch_size(img_height, img_width)
+    overlap = 3 * patch_size // 4
+
+    sam_model, device = MobileSAM.get_model(patch_size, overlap)
 
     with TemporaryFile() as tmp_file:
         with h5py.File(tmp_file, "w") as write_storage:
-            num_slices, img_height, img_width = get_stack_dims(test_image)
-            patch_size = get_patch_size(img_height, img_width)
-            overlap = 3 * patch_size // 4
-
             for slice_index in tqdm(range(num_slices)):
                 image = test_image[slice_index] if num_slices > 1 else test_image
                 slice_grp = write_storage.create_group(str(slice_index))
-                get_sam_embeddings_for_slice(
-                    image,
-                    patch_size,
-                    overlap,
-                    sam_model.image_encoder,
-                    device,
-                    slice_grp,
+                get_slice_features(
+                    image=image,
+                    patch_size=patch_size,
+                    overlap=overlap,
+                    model_adapter=sam_model.image_encoder,
+                    device=device,
+                    storage_group=slice_grp,
                 )
 
         with h5py.File(tmp_file, "r") as read_storage:
