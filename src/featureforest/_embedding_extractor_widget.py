f8b3893bcdff1a7504fa0486d2dad4830eed00ff
Mehdi Seifi <mehdiseifi@gmail.com>
2024-06-20 13:46:21 +0200

    added default storage file name based on image name and model

--- src/featureforest/_embedding_extractor_widget.py
+++ src/featureforest/_embedding_extractor_widget.py
@@ -132,8 +132,13 @@ class EmbeddingExtractorWidget(QWidget):
                 self.image_combo.setCurrentIndex(index)
 
     def save_storage(self):
+        # default storage name
+        image_layer_name = self.image_combo.currentText()
+        model_name = self.model_combo.currentText()
+        storage_name = f"{image_layer_name}_{model_name}.hdf5"
+        # open the save dialog
         selected_file, _filter = QFileDialog.getSaveFileName(
-            self, "Jug Lab", ".", "Embeddings Storage(*.hdf5)"
+            self, "Jug Lab", storage_name, "Embeddings Storage(*.hdf5)"
         )
         if selected_file is not None and len(selected_file) > 0:
             if not selected_file.endswith(".hdf5"):
