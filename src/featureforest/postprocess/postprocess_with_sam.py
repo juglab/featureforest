fixed bug of image type and normalization

src/featureforest/postprocess/postprocess_with_sam.py
@@ -222,6 +222,11 @@ def get_sam_auto_masks(input_image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]
             3, axis=-1
         )
     assert is_image_rgb(input_image)
+    # normalize the image in [0, 255] uint8
+    image = input_image.copy()
+    _min = image.min()
+    _max = image.max()
+    image = ((image - _min) * (255 / (_max - _min))).astype(np.uint8)
     # init a sam auto-segmentation mask generator
     mask_generator = SamAutomaticMaskGenerator(
         model=get_light_hq_sam(),
@@ -237,7 +242,7 @@ def get_sam_auto_masks(input_image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]
     # generate SAM masks
     print("generating masks using SamAutomaticMaskGenerator...")
     with np_progress(range(1), desc="Generating masks using SamAutomaticMaskGenerator"):
-        sam_generated_masks = mask_generator.generate(input_image)
+        sam_generated_masks = mask_generator.generate(image)
     sam_masks = np.array([mask["segmentation"] for mask in sam_generated_masks])
     sam_areas = np.array([mask["area"] for mask in sam_generated_masks])
 

src/featureforest/utils/extract.py
@@ -32,7 +32,11 @@ def get_slice_features(
         storage_group (h5py.Group): _description_
     """
     # image to torch tensor
-    img_data = torch.from_numpy(image).to(torch.float32) / 255.0
+    img_data = torch.from_numpy(image.copy()).to(torch.float32)
+    # normalize in [0, 1]
+    _min = img_data.min()
+    _max = img_data.max()
+    img_data = (img_data - _min) / (_max - _min)
     # for sam the input image should be 4D: BxCxHxW ; an RGB image.
     if is_image_rgb(image):
         # it's already RGB, put the channels first and add a batch dim.
@@ -61,7 +65,7 @@ def get_slice_features(
     )
 
     # get sam encoder output for image patches
-    print("\nextracting features:")
+    print("extracting features:")
     for b_idx in np_progress(range(num_batches), desc="extracting feature:"):
         print(f"batch #{b_idx + 1} of {num_batches}")
         start = b_idx * batch_size
@@ -105,7 +109,7 @@ def extract_embeddings_to_file(
         for slice_index in np_progress(
             range(num_slices), desc="extract features for slices"
         ):
-            print(f"slice index: {slice_index}")
+            print(f"\nslice index: {slice_index}")
             slice_img = image[slice_index] if num_slices > 1 else image
             slice_grp = storage.create_group(str(slice_index))
             for _ in get_slice_features(