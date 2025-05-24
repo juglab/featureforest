a3e71093368459bfe8a00e66d6c948d28249a123
Mehdi Seifi <mehdiseifi@gmail.com>
2025-05-24 16:00:15 +0200

    changed the environment name to featureforest

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 # This script will install FeatureForest and its dependencies in a new conda environment.
 echo "\nCreate a new conda environment named featureforest with Python 3.10"
 conda init
-conda create -n test_ff -y python=3.10
+conda create -n featureforest -y python=3.10
 echo "\nActivate the featureforest environment"
 source activate base
-conda activate test_ff
+conda activate featureforest
 echo "\nInstall PyTorch and torchvision using light-the-torch"
 pip install light-the-torch
 ltt install 'torch>=2.5.1' 'torchvision>=0.20.1'
