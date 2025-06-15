6393b2afa9a7cb719b92b4674b929545e9b9f8a0
Mehdi Seifi <mehdiseifi@gmail.com>
2025-06-15 16:23:20 +0200

    updated run_pipeline script

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,143 +1,22 @@
 import argparse
-import multiprocessing as mp
 import pickle
 import time
-from collections.abc import Generator
 from pathlib import Path
 
 import numpy as np
-import torch
-from PIL import Image, ImageSequence
-from sklearn.ensemble import RandomForestClassifier as RF
+import pims
+import tifffile
 
-from featureforest.models import BaseModelAdapter, get_available_models, get_model
-from featureforest.models.SAM import SAMAdapter
+from featureforest.models import get_available_models, get_model
+
+# from featureforest.models.SAM import SAMAdapter
 from featureforest.postprocess import (
     get_sam_auto_masks,
     postprocess,
     postprocess_with_sam,
     postprocess_with_sam_auto,
 )
-from featureforest.utils.data import (
-    get_num_patches,
-    get_stride_margin,
-    is_image_rgb,
-    patchify,
-)
-
-
-def predict_patches(
-    patch_features: np.ndarray,
-    rf_model: RF,
-    model_adapter: BaseModelAdapter,
-    batch_idx: int,
-    result_dict: dict,
-) -> None:
-    """Predicts the class labels for a given set of patch features.
-
-    Args:
-        patch_features (np.ndarray): Patch features to be predicted.
-        rf_model (RF): Random Forest Model used for predictions.
-        model_adapter (BaseModelAdapter): Model adapter object used for extracting data.
-        batch_idx (int): Batch index of the current patch features.
-        result_dict (dict): Dictionary where the predicted masks will be stored.
-    """
-    patch_masks = []
-    # shape: N x target_size x target_size x C
-    num_patches = patch_features.shape[0]
-    total_channels = model_adapter.get_total_output_channels()
-    print(f"predicting {num_patches} patches...")
-    for i in range(num_patches):
-        patch_data = patch_features[i].reshape(-1, total_channels)
-        pred = rf_model.predict(patch_data).astype(np.uint8)
-        patch_masks.append(pred)
-
-    patch_masks = np.vstack(patch_masks)
-    result_dict[batch_idx] = patch_masks
-
-
-def get_image_mask(
-    patch_masks: np.ndarray,
-    img_height: int,
-    img_width: int,
-    patch_size: int,
-    overlap: int,
-) -> np.ndarray:
-    """Gets the final image mask by combining the individual patch masks.
-
-    Args:
-        patch_masks (ndarray): Patch masks to combine into an image mask.
-        img_height (int): Height of the input image.
-        img_width (int): Width of the input image.
-        patch_size (int): Size of the patches.
-        overlap (int): Overlap between adjacent patches.
-
-    Returns:
-        np.ndarray: Final image mask.
-    """
-    stride, _ = get_stride_margin(patch_size, overlap)
-    patch_rows, patch_cols = get_num_patches(img_height, img_width, patch_size, overlap)
-    mask_image = patch_masks.reshape(patch_rows, patch_cols, stride, stride)
-    mask_image = np.moveaxis(mask_image, 1, 2).reshape(
-        patch_rows * stride, patch_cols * stride
-    )
-    # skip paddings
-    mask_image = mask_image[:img_height, :img_width]
-
-    return mask_image
-
-
-def get_slice_features(
-    image: np.ndarray, model_adapter: BaseModelAdapter
-) -> Generator[tuple[int, np.ndarray], None, None]:
-    """Extract features for one image using the given model adapter
-
-    Args:
-        image: Input image array
-        model_adapter: Model adapter to extract features from
-    Returns:
-        Generator yielding tuples containing batch index and extracted features.
-    """
-    # image to torch tensor
-    img_data = torch.from_numpy(image).to(torch.float32)
-    # normalize in [0, 1]
-    _min = img_data.min()
-    _max = img_data.max()
-    img_data = (img_data - _min) / (_max - _min)
-    # for sam the input image should be 4D: BxCxHxW ; an RGB image.
-    if is_image_rgb(image):
-        # it's already RGB, put the channels first and add a batch dim.
-        img_data = img_data[..., :3]  # ignore the Alpha channel (in case of PNG).
-        img_data = img_data.permute([2, 0, 1]).unsqueeze(0)
-    else:
-        img_data = img_data.unsqueeze(0).unsqueeze(0).expand(-1, 3, -1, -1)
-
-    # get input patches
-    patch_size = model_adapter.patch_size
-    overlap = model_adapter.overlap
-    data_patches = patchify(img_data, patch_size, overlap)
-    num_patches = len(data_patches)
-
-    # set a low batch size
-    batch_size = 8
-    # for big SAM we need even lower batch size :(
-    if isinstance(model_adapter, SAMAdapter):
-        batch_size = 2
-    num_batches = int(np.ceil(num_patches / batch_size))
-
-    # get sam encoder output for image patches
-    print("extracting slice features:")
-    for b_idx in range(num_batches):
-        print(f"batch #{b_idx + 1} of {num_batches}")
-        start = b_idx * batch_size
-        end = start + batch_size
-        slice_features = model_adapter.get_features_patches(
-            data_patches[start:end].to(model_adapter.device)
-        ).cpu()
-        if isinstance(slice_features, tuple):  # model with more than one output
-            slice_features = torch.cat(slice_features, dim=-1)
-
-        yield b_idx, slice_features.numpy()
+from featureforest.utils.pipeline_prediction import run_prediction_pipeline
 
 
 def apply_postprocessing(
@@ -149,19 +28,19 @@ def apply_postprocessing(
     use_sam_predictor: bool,
     use_sam_autoseg: bool,
     iou_threshold: float,
-) -> np.ndarray:
+) -> dict:
     post_masks = {}
-    # if not use_sam_predictor and not use_sam_autoseg:
+    # simple post-processing
     mask = postprocess(
         segmentation_image, smoothing_iterations, area_threshold, area_is_absolute
     )
-    post_masks["Simple"] = mask
+    post_masks["post_simple"] = mask
 
     if use_sam_predictor:
         mask = postprocess_with_sam(
             segmentation_image, smoothing_iterations, area_threshold, area_is_absolute
         )
-        post_masks["SAMPredictor"] = mask
+        post_masks["post_sam"] = mask
 
     if use_sam_autoseg:
         sam_auto_masks = get_sam_auto_masks(input_image)
@@ -173,7 +52,7 @@ def apply_postprocessing(
             area_threshold,
             area_is_absolute,
         )
-        post_masks["SAMAutoSegmentation"] = mask
+        post_masks["post_sam_auto"] = mask
 
     return post_masks
 
@@ -183,6 +62,7 @@ def main(
     rf_model_file: str,
     output_dir: str,
     model_name: str = "SAM2_Large",
+    no_patching: bool = False,
     smoothing_iterations: int = 25,
     area_threshold: int = 50,
     use_sam_predictor: bool = True,
@@ -198,16 +78,14 @@ def main(
     # result folder
     segmentation_dir = Path(output_dir)
     segmentation_dir.mkdir(parents=True, exist_ok=True)
-    prediction_dir = segmentation_dir.joinpath("Prediction")
+    prediction_dir = segmentation_dir.joinpath("prediction")
     prediction_dir.mkdir(exist_ok=True)
+    simple_post_dir = segmentation_dir.joinpath("post_simple")
+    simple_post_dir.mkdir(parents=True, exist_ok=True)
+    sam_post_dir = segmentation_dir.joinpath("post_sam")
+    sam_post_dir.mkdir(parents=True, exist_ok=True)
 
-    # get input image dims
-    input_stack = Image.open(data_path)
-    num_slices = input_stack.n_frames
-    img_height = input_stack.height
-    img_width = input_stack.width
-    print(f"input_stack: {num_slices}, {img_height}, {img_width}")
-
+    # load rf model
     with open(rf_model_path, mode="rb") as f:
         model_data = pickle.load(f)
     # compatibility check for old format rf model
@@ -221,13 +99,17 @@ def main(
     rf_model.set_params(verbose=0)
     print(rf_model)
 
+    # get stack dims
+    lazy_stack = pims.open(input_file)
+    img_height, img_width = lazy_stack.frame_shape
+
     # list of available models
     available_models = get_available_models()
     assert model_name in available_models, (
         f"Couldn't find {model_name} in available models\n{available_models}."
     )
-
     model_adapter = get_model(model_name, img_height, img_width)
+    model_adapter.no_patching = no_patching
     patch_size = model_adapter.patch_size
     overlap = model_adapter.overlap
     print(f"patch_size: {patch_size}, overlap: {overlap}")
@@ -238,41 +120,23 @@ def main(
     use_sam_autoseg = False
     sam_autoseg_iou_threshold = 0.35
 
-    # ### Prediction
+    # ### Prediction ###
     tic = time.perf_counter()
-    for i, page in enumerate(ImageSequence.Iterator(input_stack)):
-        print(f"\nslice {i + 1}")
-        slice_img = np.array(page.convert("RGB"))
-        procs = []
-        # random forest prediction happens per batch of extracted features
-        # in a separate process.
-        with mp.Manager() as manager:
-            result_dict = manager.dict()
-            for b_idx, patch_features in get_slice_features(slice_img, model_adapter):
-                proc = mp.Process(
-                    target=predict_patches,
-                    args=(patch_features, rf_model, model_adapter, b_idx, result_dict),
-                )
-                procs.append(proc)
-                proc.start()
-            # wait until all processes are done
-            for p in procs:
-                if p.is_alive:
-                    p.join()
-            # collect results from each process
-            batch_indices = sorted(result_dict.keys())
-            patch_masks = [result_dict[b] for b in batch_indices]
-            patch_masks = np.vstack(patch_masks)
-            slice_mask = get_image_mask(
-                patch_masks, img_height, img_width, patch_size, overlap
-            )
-
-        img = Image.fromarray(slice_mask)
-        img.save(prediction_dir.joinpath(f"slice_{i:04}_prediction.tiff"))
+    for slice_mask, idx, total in run_prediction_pipeline(
+        input_stack=input_file,
+        model_adapter=model_adapter,
+        rf_model=rf_model,
+    ):
+        print(f"\nslice {idx + 1} / {total}")
+        tifffile.imwrite(
+            prediction_dir.joinpath(f"slice_{idx:04}_prediction.tiff"), slice_mask
+        )
 
         if do_postprocess:
+            print("\nrunning post-processing...")
+            slice_img = lazy_stack[idx]
             post_masks = apply_postprocessing(
-                slice_img,
+                slice_img,  # type: ignore
                 slice_mask,
                 smoothing_iterations,
                 area_threshold,
@@ -283,10 +147,9 @@ def main(
             )
             # save results
             for name, mask in post_masks.items():
-                img = Image.fromarray(mask)
                 seg_dir = segmentation_dir.joinpath(name)
-                seg_dir.mkdir(exist_ok=True)
-                img.save(seg_dir.joinpath(f"slice_{i:04}_{name}.tiff"))
+                # seg_dir.mkdir(exist_ok=True)
+                tifffile.imwrite(seg_dir.joinpath(f"slice_{idx:04}_{name}.tiff"), mask)
 
     print(f"total elapsed time: {(time.perf_counter() - tic)} seconds")
 
@@ -304,6 +167,11 @@ if __name__ == "__main__":
         help="Name of the model for feature extraction",
     )
     parser.add_argument(
+        "--no_patching",
+        action="store_true",
+        help="If true, no patching will be used during feature extraction",
+    )
+    parser.add_argument(
         "--smoothing_iterations",
         default=25,
         type=int,
@@ -319,7 +187,7 @@ if __name__ == "__main__":
         "--use_sam_predictor",
         default=True,
         action="store_true",
-        help="To use SAM2 for generating final masks",
+        help="uses SAM2 for generating final masks",
     )
 
     args = parser.parse_args()
@@ -329,6 +197,7 @@ if __name__ == "__main__":
         rf_model_file=args.rf_model,
         output_dir=args.outdir,
         model_name=args.feat_model,
+        no_patching=args.no_patching,
         smoothing_iterations=args.smoothing_iterations,
         area_threshold=args.area_threshold,
         use_sam_predictor=args.use_sam_predictor,
